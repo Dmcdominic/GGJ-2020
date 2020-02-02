d293141ca1f2ac22365c93516c7d1551fc328b1b
Thomas Carey <tcarey@andrew.cmu.edu>
2020-02-02 12:05:46 -0500

    explosion effect

--- GGJ 2020/Assets/Scripts/Car/DiesFromKillzone.cs
+++ GGJ 2020/Assets/Scripts/Car/DiesFromKillzone.cs
@@ -6,8 +6,18 @@ public class DiesFromKillzone : MonoBehaviour {
 
     public IntEvent playerDied;
 
-    private bool diedThisFrame = false;
+    public GameObject explosionEffect;
 
+    private bool diedThisFrame = false;
+    private int playerId;
+
+    private car_parts parts;
+
+    private void Start()
+    {
+        parts = GetComponentInParent<car_parts>();
+        playerId = GetComponentInParent<playerID>().p;
+    }
 
     private void OnTriggerEnter(Collider collider) {
         if (diedThisFrame) {
@@ -18,6 +28,19 @@ public class DiesFromKillzone : MonoBehaviour {
             diedThisFrame = true;
             Destroy(gameObject);
             // TODO - EXPLODE
+        }
+    }
+
+    private void Update()
+    {
+        if(parts.partCount(playerId, part.engine) < 1)
+        {
+            GameObject expl = GameObject.Instantiate(explosionEffect);
+            expl.transform.position = gameObject.transform.position;
+            
+            playerDied.Invoke(GetComponentInParent<playerID>().p);
+            diedThisFrame = true;
+            Destroy(gameObject);
         }
     }
 }
