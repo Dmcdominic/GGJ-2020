60792675f0e2adb232085cb88fc754a4c44b4ae8
Malek <maleka@qatar.cmu.edu>
2020-02-02 07:49:49 +0300

    Vibrations and Sparks

--- GGJ 2020/Assets/Scripts/Input/InputController.cs
+++ GGJ 2020/Assets/Scripts/Input/InputController.cs
@@ -24,6 +24,12 @@ public class InputController : MonoBehaviour
     private bool leftshoulderpressed;
     private bool rightshoulderpressed;
 
+    private float vibration = 0;
+    private float vibration_standby = 0.05f;
+    private float vibration_move = 0.1f;
+    private float vibration_boost = 0.5f;
+    private float vibration_break = 0.3f;
+
     private void Awake()
     {
 #if UNITY_STANDALONE_WIN
@@ -48,23 +54,40 @@ public class InputController : MonoBehaviour
         playerControlInfo.horn = GamePad.GetState(player).Buttons.A == ButtonState.Pressed;
         state[(int) player] = playerControlInfo;
 
-        if (!leftshoulderpressed && GamePad.GetState(player).Buttons.LeftShoulder == ButtonState.Pressed)
+
+
+        if (!leftshoulderpressed && GamePad.GetState(player).Triggers.Left > 0)
         {
             leftshoulderpressed = true;
             state[(int) player].shiftDown.Invoke();
         }
-        if(leftshoulderpressed && GamePad.GetState(player).Buttons.LeftShoulder == ButtonState.Released)
+        if(leftshoulderpressed && GamePad.GetState(player).Triggers.Left == 0)
         {
             leftshoulderpressed = false;
         }
-        if (!rightshoulderpressed && GamePad.GetState(player).Buttons.RightShoulder == ButtonState.Pressed)
+        if (!rightshoulderpressed && GamePad.GetState(player).Triggers.Right > 0)
         {
             rightshoulderpressed = true;
             state[(int) player].shiftDown.Invoke();
         }
-        if(rightshoulderpressed && GamePad.GetState(player).Buttons.RightShoulder == ButtonState.Released)
+        if(rightshoulderpressed && GamePad.GetState(player).Triggers.Right == 0)
         {
             rightshoulderpressed = false;
+        }
+
+        if (leftshoulderpressed)
+        {
+            XInputDotNetPure.GamePad.SetVibration(player, vibration_break, 0);
+        } else if (rightshoulderpressed)
+        {
+            XInputDotNetPure.GamePad.SetVibration(player, 0, vibration_boost);
+        } else if(playerControlInfo.direction != new Vector3())
+        {
+            XInputDotNetPure.GamePad.SetVibration(player, vibration_move, vibration_standby);
+        }
+        else
+        {
+            XInputDotNetPure.GamePad.SetVibration(player, vibration_standby, vibration_standby);
         }
 #endif
     }
